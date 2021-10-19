b2f4ddd9ff2770a3864bc76e405e30b404b347db
Eduardo Galera <galera.eduardo@gmail.com>
2021-10-19 16:20:49 -0300

    Trabajo final con formulario de registro

---
+++ form-script.js
@@ -0,0 +1,63 @@
+function validarFormulario(){
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let dni = document.getElementById("dni").value;
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+    let selector = document.getElementById("selector").selectedIndex;
+    let cp = document.getElementById("cp").value;
+    let correo = document.getElementById("correo").value;
+    let fecha = document.getElementById("fecha").value;
+    let boleana = false;
+  
+    
+    if(!nombre || nombre.length == 0){
+        alert("Error: El campo nombre no debe ir vacio o con espacios en blanco");
+        return false;
+    }
+
+    if(!apellido || apellido.length == 0){
+        alert("Error: El campo nombre no debe ir vacio o con espacios en blanco");
+        return false;
+    }
+    
+
+    if(!dni || dni.length == 0 || isNaN(dni)){
+        alert("Error: Debe ingresar un DNI");
+        return false;
+    }
+    if(!direccion || direccion.length == 0){
+        alert("Error: El campo nombre no debe ir vacio o con espacios en blanco");
+        return false;
+    }
+
+    if(!telefono || telefono.length == 0 || isNaN(telefono)){
+        alert("Error: Debe ingresar un número de telefono");
+        return false;
+    }
+
+    if(!selector || selector == 0){
+        alert("Error: Debe selecionar una opción del selector");
+        return false;
+    }
+    if(!cp || cp.length == 0 || isNaN(cp)){
+        alert("Error: Debe ingresar un número de telefono");
+        return false;
+    }
+    if((!(/\S+@\S+\.\S+/.test(correo)))){
+        alert("Error: Escribir un correo válido");
+        return false;
+    }
+
+    if(!isNaN(fecha)){
+        alert("Error: Debe elegir una fecha");
+        return false;
+    }
+
+    
+
+ return true;   
+}
+
+console.log("Hola Mundo")
